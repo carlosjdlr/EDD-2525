1987cd6bcdfeeaf14b90c1c52274ab45f902a49c
carlosjdlr <cj.maciasc@uea.edu.ec>
2025-06-29 12:12:18 -0500

    Mostrar cantidad total

--- semana5.0/Program.cs
+++ semana5.0/Program.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-
-class Program
-{
-    static void Main()
-    {
-        List<Asignatura> asignaturas = new List<Asignatura>
+﻿
+List<Asignatura> asignaturas = new List<Asignatura>
         {
             new Asignatura("Matemáticas"),
             new Asignatura("Física"),
@@ -14,23 +8,28 @@ class Program
             new Asignatura("Lengua")
         };
 
-        Console.WriteLine("¿Deseas agregar otra asignatura? (s/n)");
-        string respuesta = Console.ReadLine()?.ToLower();
+Console.WriteLine("¿Deseas agregar otra asignatura? (s/n)");
+string respuesta = Console.ReadLine()?.ToLower();
 
-        while (respuesta == "s")
-        {
-            Console.Write("Ingresa el nombre de la asignatura: ");
-            string nuevaAsignatura = Console.ReadLine();
-            if (!string.IsNullOrWhiteSpace(nuevaAsignatura))
-            {
-                asignaturas.Add(new Asignatura(nuevaAsignatura));
-            }
-
-            Console.WriteLine("¿Deseas agregar otra asignatura? (s/n)");
-            respuesta = Console.ReadLine()?.ToLower();
-        }
-
-        asignaturas.Sort((a, b) => a.Nombre.CompareTo(b.Nombre));
-
+while (respuesta == "s")
+{
+    Console.Write("Ingresa el nombre de la asignatura: ");
+    string nuevaAsignatura = Console.ReadLine() ?? string.Empty;
+    if (!string.IsNullOrWhiteSpace(nuevaAsignatura))
+    {
+        asignaturas.Add(new Asignatura(nuevaAsignatura));
     }
+
+    Console.WriteLine("¿Deseas agregar otra asignatura? (s/n)");
+    respuesta = Console.ReadLine()?.ToLower();
 }
+
+asignaturas.Sort((a, b) => a.Nombre.CompareTo(b.Nombre));
+
+Console.WriteLine("\nAsignaturas ordenadas alfabéticamente:");
+foreach (var asignatura in asignaturas)
+{
+    Console.WriteLine($"- {asignatura}");
+}
+
+Console.WriteLine($"\nTotal de asignaturas: {asignaturas.Count}");
--- semana5.0/obj/Debug/net9.0/semana5.0.AssemblyInfo.cs
+++ semana5.0/obj/Debug/net9.0/semana5.0.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("semana5.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c9ffb7493b4f89cff8ed9266d035747fcbae93d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd4c15ae5c33c08d3bb5ba7d09c6866a7fe97792")]
 [assembly: System.Reflection.AssemblyProductAttribute("semana5.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("semana5.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
