4c9ffb7493b4f89cff8ed9266d035747fcbae93d
carlosjdlr <cj.maciasc@uea.edu.ec>
2025-06-29 12:06:03 -0500

    Permitir agregar asignaturas desde consola

--- semana5.0/Program.cs
+++ semana5.0/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<Asignatura> asignaturas = new List<Asignatura>
+        {
+            new Asignatura("Matemáticas"),
+            new Asignatura("Física"),
+            new Asignatura("Química"),
+            new Asignatura("Historia"),
+            new Asignatura("Lengua")
+        };
+
+        Console.WriteLine("¿Deseas agregar otra asignatura? (s/n)");
+        string respuesta = Console.ReadLine()?.ToLower();
+
+        while (respuesta == "s")
+        {
+            Console.Write("Ingresa el nombre de la asignatura: ");
+            string nuevaAsignatura = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(nuevaAsignatura))
+            {
+                asignaturas.Add(new Asignatura(nuevaAsignatura));
+            }
+
+            Console.WriteLine("¿Deseas agregar otra asignatura? (s/n)");
+            respuesta = Console.ReadLine()?.ToLower();
+        }
+
+       
+    }
+}
