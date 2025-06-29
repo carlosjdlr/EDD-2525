e33d749f6a3bb999069f03d5a8dc33a675a93988
carlosjdlr <cj.maciasc@uea.edu.ec>
2025-06-29 13:00:47 -0500

    asignaturas

--- guiatelefonica1/obj/Debug/net9.0/guiatelefonica1.AssemblyInfo.cs
+++ guiatelefonica1/obj/Debug/net9.0/guiatelefonica1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("guiatelefonica1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1987cd6bcdfeeaf14b90c1c52274ab45f902a49c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+352dbd11c257837e84b828033d459d628f966c43")]
 [assembly: System.Reflection.AssemblyProductAttribute("guiatelefonica1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("guiatelefonica1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ semana5/ejercicio2/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main()
+    {
+        List<string> asignaturas = new List<string>
+        {
+            "Matemáticas",
+            "Física",
+            "Química",
+            "Historia",
+            "Lengua"
+        };
+
+        
+    }
+}
