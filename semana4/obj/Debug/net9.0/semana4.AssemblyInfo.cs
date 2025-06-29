70e2a7c74ab07bfb37e3d5d9565438ab13d5384f
carlosjdlr <cj.maciasc@uea.edu.ec>
2025-06-29 13:28:16 -0500

    contar vocales

--- semana4/obj/Debug/net9.0/semana4.AssemblyInfo.cs
+++ semana4/obj/Debug/net9.0/semana4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("semana4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+512556d1faaacc03ada1610c1540f209064bd213")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+195dc21e98b6d1650575ed487c131590060aec41")]
 [assembly: System.Reflection.AssemblyProductAttribute("semana4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("semana4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- semana5/ejercicio5/ConsoleApp1/Program.cs
+++ semana5/ejercicio5/ConsoleApp1/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingresa una palabra: ");
+        string palabra = Console.ReadLine().ToLower();
+
+        int contadorA = 0, contadorE = 0, contadorI = 0, contadorO = 0, contadorU = 0;
+
+        foreach (char letra in palabra)
+        {
+            switch (letra)
+            {
+                case 'a':
+                    contadorA++;
+                    break;
+                case 'e':
+                    contadorE++;
+                    break;
+                case 'i':
+                    contadorI++;
+                    break;
+                case 'o':
+                    contadorO++;
+                    break;
+                case 'u':
+                    contadorU++;
+                    break;
+            }
+        }
+
+        Console.WriteLine("\nCantidad de vocales:");
+        Console.WriteLine($"A: {contadorA}");
+        Console.WriteLine($"E: {contadorE}");
+        Console.WriteLine($"I: {contadorI}");
+        Console.WriteLine($"O: {contadorO}");
+        Console.WriteLine($"U: {contadorU}");
+    }
+}
