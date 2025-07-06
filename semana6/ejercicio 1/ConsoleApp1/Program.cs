75ab83ad682eb66d17bb70661044b952967de03a
carlosjdlr <cj.maciasc@uea.edu.ec>
2025-07-06 15:11:54 -0500

    crear nodo

---
+++ semana6/ejercicio 1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿
+public class Nodo
+{
+    public int Valor;
+    public Nodo Siguiente;
+
+    public Nodo(int valor)
+    {
+        Valor = valor;
+        Siguiente = null;
+    }
+}
+
+public class ListaEnlazada
+{
+    public Nodo Cabeza;
+
+    public ListaEnlazada()
+    {
+        Cabeza = null;
+    }
+
+    public void AgregarAlFinal(int valor)
+    {
+        Nodo nuevoNodo = new Nodo(valor);
+        if (Cabeza == null)
+        {
+            Cabeza = nuevoNodo;
+        }
+        else
+        {
+            Nodo actual = Cabeza;
+            while (actual.Siguiente != null)
+            {
+                actual = actual.Siguiente;
+            }
+            actual.Siguiente = nuevoNodo;
+        }
+    }
+}
