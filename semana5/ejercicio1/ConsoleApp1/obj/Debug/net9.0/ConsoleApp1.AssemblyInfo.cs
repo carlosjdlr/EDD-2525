b924e54ee0dc96477c6ad0875a3346970c7332f2
carlosjdlr <cj.maciasc@uea.edu.ec>
2025-07-13 11:39:30 -0500

    torres de hanoi

--- semana5/ejercicio1/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ semana5/ejercicio1/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d22b3ad118edd839a163190605ead2343d59ea0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9526104ccac00414de9eabf8e859cdeadd726f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- semana7/resolucion/ConsoleApp1/Program.cs
+++ semana7/resolucion/ConsoleApp1/Program.cs
@@ -1,1 +1,55 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+class Torre
+{
+    public Stack<int> Discos { get; private set; }
+    public string Nombre { get; private set; }
+
+    public Torre(string nombre)
+    {
+        Nombre = nombre;
+        Discos = new Stack<int>();
+    }
+
+    public void MoverDiscoA(Torre destino)
+    {
+        int disco = Discos.Pop();
+        destino.Discos.Push(disco);
+        Console.WriteLine($"Mover disco {disco} de {Nombre} a {destino.Nombre}");
+    }
+}
+
+class TorresDeHanoi
+{
+    /// <summary>
+    /// Resuelve el problema de las Torres de Hanoi usando recursión y pilas.
+    /// </summary>
+    public static void Resolver(int n, Torre origen, Torre auxiliar, Torre destino)
+    {
+        if (n == 1)
+        {
+            origen.MoverDiscoA(destino);
+            return;
+        }
+
+        Resolver(n - 1, origen, destino, auxiliar);
+        origen.MoverDiscoA(destino);
+        Resolver(n - 1, auxiliar, origen, destino);
+    }
+
+    static void Main()
+    {
+        int numeroDeDiscos = 3;
+        Torre origen = new Torre("Origen");
+        Torre auxiliar = new Torre("Auxiliar");
+        Torre destino = new Torre("Destino");
+
+        for (int i = numeroDeDiscos; i >= 1; i--)
+        {
+            origen.Discos.Push(i);
+        }
+
+        Resolver(numeroDeDiscos, origen, auxiliar, destino);
+    }
+}
