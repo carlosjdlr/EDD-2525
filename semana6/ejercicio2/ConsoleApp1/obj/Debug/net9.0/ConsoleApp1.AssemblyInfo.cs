c8571bf4d3b74f1dc755911bbe34070eb1f3db12
carlosjdlr <cj.maciasc@uea.edu.ec>
2025-07-18 04:07:04 -0500

    Clase que representa a una persona

--- semana6/ejercicio2/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ semana6/ejercicio2/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9526104ccac00414de9eabf8e859cdeadd726f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b924e54ee0dc96477c6ad0875a3346970c7332f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
