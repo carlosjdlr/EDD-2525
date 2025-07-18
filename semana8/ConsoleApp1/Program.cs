5e6893502bb7574d32b0690d224e3b8e2bfef355
carlosjdlr <cj.maciasc@uea.edu.ec>
2025-07-18 04:09:30 -0500

    Clase que representa a una persona

---
+++ semana8/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ParqueDiversiones
+{
+    // Clase que representa a una persona
+    public class Persona
+    {
+        public string Nombre { get; set; }
+
+        public Persona(string nombre)
+        {
+            Nombre = nombre;
+        }
+    }
