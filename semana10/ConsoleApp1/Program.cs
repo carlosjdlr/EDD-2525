1d4ec3ea902b93e1c4914d5a6b801959459bec4d
carlosjdlr <cj.maciasc@uea.edu.ec>
2025-08-21 23:34:04 -0500

    Añadir vacunados Pfizer y AstraZeneca

--- semana10/ConsoleApp1/Program.cs
+++ semana10/ConsoleApp1/Program.cs
@@ -3,13 +3,28 @@ class ProgramaVacunacion
 {
     static void Main()
     {
-        // 1. Crear 500 ciudadanos ficticios
+        // 1. Crear 500 ciudadanos
         HashSet<string> ciudadanos = new HashSet<string>();
         for (int i = 1; i <= 500; i++)
         {
             ciudadanos.Add("Ciudadano " + i);
         }
 
-        Console.WriteLine("Total de ciudadanos creados: " + ciudadanos.Count);
+        // 2. Crear subconjunto de 75 vacunados con Pfizer
+        HashSet<string> pfizer = new HashSet<string>();
+        for (int i = 1; i <= 75; i++)
+        {
+            pfizer.Add("Ciudadano " + i);
+        }
+
+        // 3. Crear subconjunto de 75 vacunados con AstraZeneca
+        HashSet<string> astrazeneca = new HashSet<string>();
+        for (int i = 50; i < 125; i++) // solapamiento con Pfizer
+        {
+            astrazeneca.Add("Ciudadano " + i);
+        }
+
+        Console.WriteLine("Pfizer: " + pfizer.Count);
+        Console.WriteLine("AstraZeneca: " + astrazeneca.Count);
     }
 }
--- semana10/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ semana10/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb12126ff522246c6c6459efa5484971aa6f4b9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbd90f263660a1fd774ee8ea35efff4ea5f93f48")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
