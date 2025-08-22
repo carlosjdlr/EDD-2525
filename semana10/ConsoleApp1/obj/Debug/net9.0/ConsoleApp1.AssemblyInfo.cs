d444f18a8554e1a75810034eab0d1a5238237318
carlosjdlr <cj.maciasc@uea.edu.ec>
2025-08-21 23:35:45 -0500

    Añadir vacunados Pfizer y AstraZeneca

--- semana10/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ semana10/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbd90f263660a1fd774ee8ea35efff4ea5f93f48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d4ec3ea902b93e1c4914d5a6b801959459bec4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
