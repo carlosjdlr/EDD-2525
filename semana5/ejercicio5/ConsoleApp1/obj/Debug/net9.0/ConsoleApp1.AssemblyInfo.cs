191d1ea772b41928e668be67a1f0171da18ade41
carlosjdlr <cj.maciasc@uea.edu.ec>
2025-08-21 23:31:06 -0500

    Creación de 500 ciudadanos

--- semana5/ejercicio5/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ semana5/ejercicio5/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14a3647b3cabce9e1b62e7d155f7cc5eaaff5c68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+893a5efaa93d71bec038c630e6b1b3008a20d258")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
